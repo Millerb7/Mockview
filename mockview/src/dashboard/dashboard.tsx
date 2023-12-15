9530e40c2b6c206aae07ab2773d6bad9168ef6b9
Miller <Millerjboyd@gmail.com>
2023-12-15 16:39:27 -0600

    reomved eslint

---
+++ mockview/src/dashboard/dashboard.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect, useContext } from 'react';
+
+// const dashboard: React.FC = () => {
+
+//   return (<>
+
+//   </>);
+// };
+
+// export default dashboard;
+
+export default function dashboard() {
+    return <div>
+    <div>n</div>
+    <div>n</div>
+    </div>;
+}
